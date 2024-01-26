70028868b195b66bec5c9c79ea329633491eb040
Daniil Aleshechkin <aleshechkin.daniil@gmail.com>
2024-01-25 22:36:01 -0600

    Fixed timer code

--- lab1/main.c
+++ lab1/main.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include "sttimer.h"
 
 void delay(int millis) {
     while (millis-- > 0) {
-        volatile int x = 5971;
+        volatile int x = 2;
         while (x-- > 0) {
             __asm("nop");
         }
@@ -13,7 +14,8 @@ int main(void) {
 
     // GPIO structure for port initialization
     GPIO_InitTypeDef GPIO_InitStructure;
-
+    uint16_t time = 0;
+    uint16_t startTime = 0;
     // enable clock on APB2
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,  ENABLE);
 
@@ -23,12 +25,16 @@ int main(void) {
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;   // highest speed
     GPIO_Init(GPIOA, &GPIO_InitStructure) ;             // initialize port
 
+    timer_init();
+
     // main loop
     while(1) {
+        startTime = timer_start();
         GPIO_SetBits(GPIOA, GPIO_Pin_5);    // turn the LED on
         delay(DELAY);
 
         GPIO_ResetBits(GPIOA, GPIO_Pin_1);  // turn the LED off
         delay(DELAY);
+        time = timer_stop(startTime);
     }
 }
